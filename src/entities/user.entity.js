03c6bc4310f4ac02fe3e2c1c90bd92359c0c51f5
YourAKShaw <hitme@yourakshaw.com>
2023-12-18 01:18:31 +0530

    fix(user): :bug: add missing parameter in UserEntity

--- src/entities/user.entity.js
+++ src/entities/user.entity.js
@@ -1,10 +1,11 @@
 /* eslint-disable require-jsdoc */
 
 class UserEntity {
-  constructor(username, email, password) {
+  constructor(username, email, password, role) {
     this.username = username;
     this.email = email;
     this.password = password;
+    this.role = role;
   }
 }
 
