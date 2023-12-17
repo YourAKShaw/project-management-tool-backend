6f35b011ce7012c7a6cd4f9fe401252c51bf2da9
YourAKShaw <hitme@yourakshaw.com>
2023-12-18 01:48:24 +0530

    feat(user): :sparkles: implement UserController

---
+++ src/controllers/UserController.js
@@ -0,0 +1,39 @@
+/* eslint-disable object-curly-spacing */
+/* eslint-disable indent */
+/* eslint-disable require-jsdoc */
+import createUserSchema from '../schemas/createUser.schema.js';
+import UserService from '../services/UserService.js';
+
+class UserController {
+  static async createUser(req, res, next) {
+    try {
+      const { username, email, password, role } = req.body;
+
+      // Validate request body
+      const { error: validationError } = createUserSchema.validate(req.body);
+      if (validationError) {
+        logger.log('error', validationError.message);
+        return res.status(400).json({ error: validationError.message });
+      }
+
+      // Business logic: Create user
+      const userId = await UserService.createUser(
+        username,
+        email,
+        password,
+        role,
+      );
+
+      logger.log('info', `User with id ${userId} created`);
+
+      // Respond with the created user ID
+      return res.status(201).json({ userId });
+    } catch (error) {
+      // Handle errors
+      logger.log('error', error.message);
+      return next(error);
+    }
+  }
+}
+
+export default UserController;
