a9be4452553e9fbb4cea0113d69a9ca17c53d592
YourAKShaw <hitme@yourakshaw.com>
2023-12-18 00:31:28 +0530

    feat: :card_file_box: add UserEntity

---
+++ src/entities/user.entity.js
@@ -0,0 +1,11 @@
+/* eslint-disable require-jsdoc */
+
+class UserEntity {
+  constructor(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+}
+
+export default UserEntity;
