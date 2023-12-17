1afe8c6409a502a3b2b1efc54ca4d0704bec8c84
YourAKShaw <hitme@yourakshaw.com>
2023-12-18 01:14:58 +0530

    feat(user): :sparkles: implement UserService

---
+++ src/services/UserService.js
@@ -0,0 +1,61 @@
+/* eslint-disable indent */
+/* eslint-disable require-jsdoc */
+import bcrypt from 'bcrypt';
+
+class UserService {
+  constructor(userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  async createUser(username, email, password, role) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const userEntity = {
+      username,
+      email,
+      password: hashedPassword,
+      role,
+    };
+
+    const userId = await this.userRepository.createUser(userEntity);
+    return userId;
+  }
+
+  async getUserById(userId) {
+    return await this.userRepository.getUserById(userId);
+  }
+
+  async getUserByEmail(email) {
+    const user = await this.userRepository.getUserByEmail(email);
+    return user;
+  }
+
+  async getUserByUsername(username) {
+    return await this.userRepository.getUserByUsername(username);
+  }
+
+  async isCorrectPassword(email, password) {
+    const user = await this.userRepository.getUserByEmail(email);
+
+    if (!user) {
+      return false; // User not found
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    return isPasswordCorrect;
+  }
+
+  async updateUser(userId, updatedUserData) {
+    const result = await this.userRepository.updateUser(
+      userId,
+      updatedUserData,
+    );
+    return result;
+  }
+
+  async deleteUser(userId) {
+    const result = await this.userRepository.deleteUser(userId);
+    return result;
+  }
+}
+
+export default UserService;
